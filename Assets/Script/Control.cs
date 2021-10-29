6c5dd12e174a3d267b2d717f540980fc5f6a095d
iqbaltaufiqulH <fisttaste@gmail.com>
2021-10-29 23:45:43 +0700

    Problem Solving

---
+++ Assets/Script/Control.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    private Rigidbody2D den;
+    public float kecepatan;
+    public Vector2 gerakan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        den = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gerakan.x = Input.GetAxisRaw("Horizontal");
+        gerakan.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        den.MovePosition(den.position + gerakan * kecepatan * Time.fixedDeltaTime);
+    }
+}
