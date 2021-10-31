6b31a9f3ded40e7d72a04812cb19c25b45a9ec62
iqbaltaufiqulH <fisttaste@gmail.com>
2021-10-31 22:45:01 +0700

    Problem Solving

---
+++ Assets/Script/Button.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public void P1()
+    {
+        SceneManager.LoadScene("Scene 1");
+    }
+
+    public void P2()
+    {
+        SceneManager.LoadScene("Scene 2");
+    }
+    public void P3()
+    {
+        SceneManager.LoadScene("Scene 3");
+    }
+    public void P4()
+    {
+        SceneManager.LoadScene("Scene 4");
+    }
+    public void P5()
+    {
+        SceneManager.LoadScene("Scene 5");
+    }
+    public void P6()
+    {
+        SceneManager.LoadScene("Scene 6");
+    }
+    public void P7()
+    {
+        SceneManager.LoadScene("Scene 7");
+    }
+    public void P8()
+    {
+        SceneManager.LoadScene("Scene 8");
+    }
+    public void P9()
+    {
+        SceneManager.LoadScene("Scene 9");
+    }
+}
---
+++ Assets/Script/RandomSpawnV.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawnV : MonoBehaviour
+{
+    public GameObject Object;
+    public int banyakobject;
+
+    private void Start()
+    {
+        for (int i = 0; i < banyakobject; i++)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(5, -5), Random.Range(4f, -4f));
+            Instantiate(Object, randomPos, Quaternion.identity);
+        }
+    }
+}
