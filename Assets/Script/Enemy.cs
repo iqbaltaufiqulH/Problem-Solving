9b735a5ee6a436396c0e064a5739ca9f2fa67c29
iqbaltaufiqulH <fisttaste@gmail.com>
2021-10-31 20:17:22 +0700

    Problem Solving

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            StartCoroutine(kill());
+        }
+        if (collision.gameObject.tag == "wall")
+        {
+            StartCoroutine(kill());
+        }
+    }
+    
+    IEnumerator kill()
+    {
+        sprite.enabled = false;
+        yield return new WaitForSeconds(3);
+        transform.position = new Vector3(Random.Range(8, -8), Random.Range(4.5f, -4.5f));
+        sprite.enabled = true;
+
+    }
+}
